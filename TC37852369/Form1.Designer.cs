3833da64f186bc1144056550465e1b81c4f6e80d
entorius <eligijus.puplinskas@gmail.com>
2019-11-10 18:38:24 +0200

    Add project files.

---
+++ TC37852369/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TC37852369
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox_ID = new System.Windows.Forms.TextBox();
+            this.TextBox_Name = new System.Windows.Forms.TextBox();
+            this.TextBox_Address = new System.Windows.Forms.TextBox();
+            this.TextBox_Age = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Button_Insert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox_ID
+            // 
+            this.TextBox_ID.Location = new System.Drawing.Point(285, 63);
+            this.TextBox_ID.Name = "TextBox_ID";
+            this.TextBox_ID.Size = new System.Drawing.Size(165, 22);
+            this.TextBox_ID.TabIndex = 0;
+            // 
+            // TextBox_Name
+            // 
+            this.TextBox_Name.Location = new System.Drawing.Point(285, 116);
+            this.TextBox_Name.Name = "TextBox_Name";
+            this.TextBox_Name.Size = new System.Drawing.Size(165, 22);
+            this.TextBox_Name.TabIndex = 1;
+            // 
+            // TextBox_Address
+            // 
+            this.TextBox_Address.Location = new System.Drawing.Point(285, 168);
+            this.TextBox_Address.Name = "TextBox_Address";
+            this.TextBox_Address.Size = new System.Drawing.Size(165, 22);
+            this.TextBox_Address.TabIndex = 2;
+            // 
+            // TextBox_Age
+            // 
+            this.TextBox_Age.Location = new System.Drawing.Point(285, 221);
+            this.TextBox_Age.Name = "TextBox_Age";
+            this.TextBox_Age.Size = new System.Drawing.Size(165, 22);
+            this.TextBox_Age.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "age";
+            // 
+            // Button_Insert
+            // 
+            this.Button_Insert.Location = new System.Drawing.Point(285, 286);
+            this.Button_Insert.Name = "Button_Insert";
+            this.Button_Insert.Size = new System.Drawing.Size(121, 23);
+            this.Button_Insert.TabIndex = 8;
+            this.Button_Insert.Text = "Insert";
+            this.Button_Insert.UseVisualStyleBackColor = true;
+            this.Button_Insert.Click += new System.EventHandler(this.Button_Insert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Button_Insert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox_Age);
+            this.Controls.Add(this.TextBox_Address);
+            this.Controls.Add(this.TextBox_Name);
+            this.Controls.Add(this.TextBox_ID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_ID;
+        private System.Windows.Forms.TextBox TextBox_Name;
+        private System.Windows.Forms.TextBox TextBox_Address;
+        private System.Windows.Forms.TextBox TextBox_Age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Button_Insert;
+    }
+}
+
---
+++ TC37852369/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace TC37852369
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "mtmfz13yNJJLuio1yYMKH1jbSQRIV5anaTXv5C4O",
+            BasePath = "https://ticketbase-36d66.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if (client != null)
+            {
+                MessageBox.Show("connection established");
+            }
+        }
+
+        private async void Button_Insert_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = TextBox_ID.Text,
+                Name = TextBox_Name.Text,
+                Address = TextBox_Address.Text,
+                Age = TextBox_Age.Text
+            };
+
+            SetResponse response = await client.SetTaskAsync("User/" + TextBox_ID.Text,data);
+            Data result = response.ResultAs<Data>();
+            MessageBox.Show("Data Inserted " + result.Id);
+        }
+    }
+}
